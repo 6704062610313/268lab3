35f902d96016f3aa496c7a1f5b0ba005f625c5f8
6704062610313 <s6704062610313@email.kmutnb.ac.th>
2025-12-16 18:20:54 +0700

    Lab3 n.6
    ข้อ 6

---
+++ Lab3n6.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main() {
+    int score,n;
+
+    scanf("%d", &score);
+
+    if (score >= 68) {
+	   	if (score > 100) {
+        	printf("error score\n");
+    	}else if (score >= 85) {
+                printf("A\n");
+		}else if (score >= 75) {
+                printf("B\n");
+		}else {
+			printf("C\n");
+			n = 75-score;
+			printf("%d\n",n);
+        }
+    }
+    else {
+        if (score >= 55) {
+            printf("D\n");
+    	}
+        else if (score < 0){
+            printf("error score\n");
+        }else printf("F\n");
+    }
+
+    return 0;
+}
+
